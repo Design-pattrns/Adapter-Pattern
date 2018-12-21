971ab6c6b69ef8fc56c89e08acce069d869fdfb1
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-12-21 08:17:11 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
         Sparrow sparrow = new Sparrow();
         PlasticToyDuck toyDuck = new PlasticToyDuck();
 
-        // Wrap a bird in a birdAdapter so that it
-        // behaves like toy duck
+
         BirdAdapter birdAdapter = new BirdAdapter(sparrow);
 
         System.out.println("Sparrow...");
@@ -15,7 +14,6 @@ public class Main {
         System.out.println("ToyDuck...");
         toyDuck.squeak();
 
-        // toy duck behaving like a bird
         System.out.println("BirdAdapter...");
         birdAdapter.squeak();
     }
